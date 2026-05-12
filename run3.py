6bb11b68db482c1a25a3cc8ed8dbddc711e1a8c8
Matsumoto Daichi <e245759@ie.u-ryukyu.ac.jp>
2026-05-12 09:52:26 +0900

    week4 4th

--- run3.py
+++ run3.py
@@ -1,4 +1,10 @@
 import regression
+import datasets
 
-model = regression.RidgeRegression()
+X, Y = datasets.load_liner_example1()
+ex_X = datasets.polynomial2_features(X)
+
+model = regression.RidgeRegression(alpha=0)
 print(f"{model.alpha=}")
+model.fit(ex_X, Y)
+print(f"{model.theta=}")
